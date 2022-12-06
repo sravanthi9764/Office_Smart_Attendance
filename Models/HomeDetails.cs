cbc8c6ee8bf0a9e6fd48679d8f3a772749d843bd
sakamuri706555 <114606921+sakamuri706555@users.noreply.github.com>
2022-12-06 08:34:13 +0530

    Add files via upload

---
+++ Models/HomeDetails.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Attendance.Models
+{
+    public class loginDetail
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string userName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+    }
+    public class RegistrationDetails
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Designation is required")]
+        public string Designation { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone no. is required")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string UPassword { get; set; }
+
+        [Required(ErrorMessage = "Confirmation Password is required")]
+        [Compare("UPassword", ErrorMessage = "Confirm password doesn't match, Type again !")]
+        [DataType(DataType.Password)]
+        public string UCPassword { get; set; }
+
+        public string Role { get; set; }
+        //public Role Roles { get; set; }
+    }
+
+    public enum Role
+    {
+        User, Admin
+    }
+    public class TimesheetDetails
+    {
+        public DateTime ToDate { get; set; }
+        public DateTime FromDate { get; set; }
+    }
+
+    public class TimesheetDet
+    {
+        public string username { get; set; }
+        public string attendancesDatetime { get; set; }
+        public string attendancesDate { get; set; }
+        public string Status { get; set; }
+        public string Remark { get; set; }
+}
+
+    public class RegularizeDetails
+    {
+        public DateTime txtRegulDate { get; set; } 
+    }
+}
