ab5c58eba19ee46e91f1dfe3ab949f1e7eb947c7
sravanthi9764 <114606470+sravanthi9764@users.noreply.github.com>
2022-12-06 08:25:46 +0530

    Add files via upload

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Attendance
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
